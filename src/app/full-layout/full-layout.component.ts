27c6f2cca8b4545c3ef0aff537203c2bc136a0a2
Nada Abdelkefi <nadaabdelkefi99@gmail.com>
2019-11-28 23:46:46 +0100

    migration

--- src/app/full-layout/full-layout.component.ts
+++ src/app/full-layout/full-layout.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./full-layout.component.scss']
 })
 export class FullLayoutComponent implements OnInit {
-  
+
   constructor() { }
 
   ngOnInit() {
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0],
+       p = /^http:/.test(d.location) ? 'http' : 'https'; 
+       if (!d.getElementById(id)) { js = d.createElement(s);
+         js.id = id; js.src = p + "://platform.twitter.com/widgets.js";
+          fjs.parentNode.insertBefore(js, fjs); }
+    }(document, "script", "twitter-wjs"));
+  
+
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
   }
 
 }
