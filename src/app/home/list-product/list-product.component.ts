9c568cc0a73e3cc3b0d0e1d2e4be63956dbe9b49
Nada Abdelkefi <nadaabdelkefi99@gmail.com>
2019-12-01 22:00:23 +0100

    adding list products

--- src/app/home/list-product/list-product.component.ts
+++ src/app/home/list-product/list-product.component.ts
@@ -34,6 +34,7 @@ export class ListProductComponent implements OnInit {
           setTimeout(
             function () {
               baseContext.loadSlider();
+              baseContext.productTab();
             }, 500
           );
 
@@ -67,6 +68,44 @@ export class ListProductComponent implements OnInit {
       maxItems: getGridSize() // use function to pull in initial value
     });
 
+  }
+
+  productTab(){
+       // store the slider in a local variable
+       var $window = jQuery(window),
+       flexslider;
+ 
+     // tiny helper function to add breakpoints
+     function getGridSize() {
+       return (window.innerWidth < 320) ? 1 :
+         (window.innerWidth < 600) ? 2 :
+           (window.innerWidth < 800) ? 3 :
+             (window.innerWidth < 900) ? 4 : 5;
+     }
+    jQuery('#product-tab .featured_carousel_tab, #product-tab .latest_carousel_tab, #product-tab .bestseller_carousel_tab, #product-tab .special_carousel_tab').flexslider({
+      animation: "slide",
+      animationLoop: false,
+	  slideshow: false,
+      itemWidth: 210,
+      minItems: getGridSize(), // use function to pull in initial value
+      maxItems: getGridSize(), // use function to pull in initial value
+	  start: function(){
+      jQuery("#product-tab .tab_content").addClass("deactive");
+      jQuery("#product-tab .tab_content:first").removeClass("deactive"); //Show first tab content
+      } });
+
+      jQuery("ul#tabs li:first").addClass("active").show(); //Activate first tab
+    //On Click Event
+      jQuery(document).ready(function(){
+      jQuery("ul#tabs li").click(function() {
+          jQuery("ul#tabs li").removeClass("active"); //Remove any "active" class
+          jQuery(this).addClass("active"); //Add "active" class to selected tab
+          jQuery("#product-tab .tab_content").hide(); 
+          var activeTab = jQuery(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content
+          jQuery(activeTab).fadeIn(); //Fade in the active content
+          return false;
+      });
+    });
   }
 
 }
